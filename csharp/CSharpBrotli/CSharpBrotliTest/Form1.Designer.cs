4c2047a030c73aa976cd9420b98e7e38ee0918f5
enginekit <enginecollection@gmail.com>
2017-05-20 07:49:59 +0700

    21: update to vs2017 project, and include sharpziblib

--- csharp/CSharpBrotli/CSharpBrotliTest/Form1.Designer.cs
+++ csharp/CSharpBrotli/CSharpBrotliTest/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 23);
+            this.button1.Size = new System.Drawing.Size(183, 71);
             this.button1.TabIndex = 0;
             this.button1.Text = "Decode test_data.zip";
             this.button1.UseVisualStyleBackColor = true;
